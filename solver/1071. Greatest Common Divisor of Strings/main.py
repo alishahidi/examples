6a5d4b6f3f586e6a192930f4559fd3dfb25490ff
Ali Shahidi <alishahidi1376@gmail.com>
2023-07-10 07:51:25 +0330

    solve leetcode 1071

--- solver/1071. Greatest Common Divisor of Strings/main.py
+++ solver/1071. Greatest Common Divisor of Strings/main.py
@@ -1,12 +1,35 @@
 def gcdOfStrings(str1: str, str2: str) -> str:
+    str1 = str1.upper()
+    str2 = str2.upper()
+    if len(str2) > len(str1):
+        str1, str2 = str2, str1
     if not str1.startswith(str2):
         return ""
-    
-    remaining_str = str1
-    while (remaining_str.startswith(str2) and not remaining_str.endswith(str2)):
-      remaining_str = remaining_str[len(str2):]
+    exported_string = ""
+    for i in str2:
+        exported_string += i
+        checked_string = ""
+        while len(checked_string) < len(str1):
+            checked_string += exported_string
+        if checked_string == str1:
+            from math import gcd
+            return str1[:gcd(len(str1), len(str2))]
+
+    return ""
   
-str1 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
-str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
+str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX"
+str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
+
+"""
+ABCABC
+ABC
+ABCABCABC
+
+ABABAB
+ABABABAB
+
+
+
+"""
 
 print(gcdOfStrings(str1, str2))
