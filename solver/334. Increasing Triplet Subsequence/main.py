9accab83ea3c95a06169d84efca6b40d84abd0ea
Ali Shahidi <alishahidi1376@gmail.com>
2023-07-13 15:20:30 +0330

    solve leetcode 334

---
+++ solver/334. Increasing Triplet Subsequence/main.py
@@ -0,0 +1,22 @@
+def increasingTriplet(nums) -> bool:
+    length = len(nums)
+    if length < 3:
+        return False
+
+    first = second = float('inf')
+
+    for num in nums:
+        if num <= first:
+            first = num
+        elif num <= second:
+            second = num
+        else:
+            return True
+
+    return False    
+
+print(increasingTriplet([5,4,3,2,1]))
+
+first = 1
+second = 5
+
